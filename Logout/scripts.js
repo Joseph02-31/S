f9a2df020eb7770f6329d0477d451b7959cf3c64
Joseph02-31 <jodark2002@gmail.com>
2025-01-18 23:40:25 +0100

    Update scripts.js

--- Logout/scripts.js
+++ Logout/scripts.js
@@ -1,37 +1,6 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
-const iconClose = document.querySelector('.icon-close');
-
-const homeLink = document.querySelector('.navigation a[href="#"]'); // Select Home link
 
 
-registerLink.addEventListener('click',() => {
-    wrapper.classList.add('active');
-});
 
-loginLink.addEventListener('click',() => {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click',() => {
-    wrapper.classList.add('active-popup');
-});
-iconClose.addEventListener('click',() => {
-    wrapper.classList.remove('active-popup');
-});
-
-// Hide popup when Home is clicked
-homeLink.addEventListener('click', () => {
-    wrapper.classList.remove('active-popup');
-});
-
-            document.addEventListener("DOMContentLoaded", () => {
-                const email = sessionStorage.getItem('email'); // Get the email from session storage
-                const textDiv = document.querySelector('.text-section'); // Select the text-section div
-                const mCard = document.getElementById('m-card'); // Select the M card element
-    
                 // Check the email and display the corresponding message
     if (email === "Youssef@pi.m") {
         textDiv.textContent = "Welcome Mr. Youssef BOUZIANI !"; // Message for ab@pi.m
