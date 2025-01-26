9ecd09ca17b4272100a80ff0c423017050129a96
Joseph02-31 <jodark2002@gmail.com>
2025-01-26 19:36:02 +0100

    Update scripts.js

--- Logout/scripts.js
+++ Logout/scripts.js
@@ -88,3 +88,17 @@ function toggleDropdown() {
 
 // Apply the saved language when the page loads
 document.addEventListener("DOMContentLoaded", applySavedLanguage);
+
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
